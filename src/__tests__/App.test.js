326283dfaa3e7488ae6917ed290c58fa61da7cb0
Chris Edwards <chrisedwards96@outlook.com>
2023-04-11 16:38:07 +0100

    CSS Improvements

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../components/App";
 
 describe("App", () => {
-  const { asFragment } = render(<App />);
-
   it("renders correctly", () => {
+    const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders NASA logo", () => {
+    render(<App />);
+    const logoElement = screen.getByAltText(/Nasa Logo/i);
+    expect(logoElement).toBeInTheDocument();
   });
 });
